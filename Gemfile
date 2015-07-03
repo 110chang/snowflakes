b65e1373bf425a9f6dea0560d3a364f66e506d05
110chang <u@110chang.com>
2015-07-03 18:08:02 +0900

    [add] sass environment

--- Gemfile
+++ Gemfile
@@ -1,5 +1,11 @@
 # A sample Gemfile
-source "https://rubygems.org"
+source 'https://rubygems.org'
+#source 'https://rails-assets.org'
 
 # gem "rails"
 gem 'sinatra'
+gem 'sinatra-contrib'
+
+gem 'slim'
+gem 'sass'
+gem 'compass'
--- web.rb
+++ web.rb
@@ -1,5 +1,22 @@
 require 'sinatra'
+#require 'sinatra/reloader' if development?
+require 'compass'
+
+configure do
+  Compass.configuration do |config|
+    config.project_path = File.dirname(__FILE__)
+    config.sass_dir = 'views'
+  end
+  set :sass, Compass.sass_engine_options
+  set :scss, Compass.sass_engine_options
+end
 
 get '/' do
-  "Hello, world"
+  slim :index
+end
+
+get '/css/*.css' do
+  content_type 'text/css', :charset => 'utf-8'
+  filename = params[:splat].first
+  sass filename.to_sym, :views => "#{settings.root}/assets/css"
 end
